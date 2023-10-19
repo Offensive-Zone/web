d1388b25e2a1e80398c00f6d7af8004b94c3a67b
Edgardo Krause <edgardo.krause@gmail.com>
2023-10-19 17:28:01 -0500

    updates

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,36 @@
 import { Box, Container, List, ListItem, Typography } from "@mui/material"
+import TypeFormEmbed from './TypeFormEmbed'; // Ajusta la ruta según tu estructura
+
 
 const Footer = ()=>{
     return (
+
+
+       
+
         <Box  sx={{backgroundColor:'primary.main', width:'100%', height:{xs:'25vh', lg:'25vh'}, position:'relative', bottom:0, p:'20px 0 20px 0'}}>
+
+
+
+
             <Container maxWidth='lg'>
+
+                
+
+           
+            
+            
             <Box sx={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+          
                 <Box>
-                    <Typography color='white' variant="h5">Redes Sociales</Typography>
+                    <Typography color='white' variant="h5">Social Media</Typography>
                     <List sx={{color:'white', listStyleType:'none'}}>
                         <ListItem><a href="https://www.linkedin.com/company/offensive-security-services">Linkedin</a></ListItem>
                         <ListItem><a href="https://www.instagram.com/offensivesecurityservices/">Instagram</a></ListItem>
                     </List>
                 </Box>
                 <Box>
-                <Typography color='white' component="h3" variant="h5">Contacto</Typography>
+                <Typography color='white' component="h3" variant="h5">Contact info</Typography>
                     <List sx={{color:'white', listStyleType:'none'}}>
                     <ListItem><a href="mailto:info@offensivesecurityservices.com">info@offensivesecurityservices.com</a></ListItem>
                     <ListItem>Santander,Colombia</ListItem>
@@ -24,9 +41,15 @@ const Footer = ()=>{
                     </List>
                 </Box>
             </Box>
+
+          
             </Container>
+            <TypeFormEmbed /> 
         </Box>
     )
 }
+
+
+
 
 export default Footer
